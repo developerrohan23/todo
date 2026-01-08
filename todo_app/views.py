6099078257618ee45b2d5cb9e53a6b3c0e578518
rohan kumar singh <developer.rohan23@gmail.com>
2026-01-08 19:32:11 +0530

    add functionality

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Task
+def add_task(request):
+    task=request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
